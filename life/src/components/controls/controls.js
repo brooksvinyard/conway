3a8dd56f44ac0648a343398a284d6600a01f0297
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-07-18 15:47:20 -0700

    Breakpoints for mobile

--- life/src/components/controls/controls.js
+++ life/src/components/controls/controls.js
@@ -6,18 +6,24 @@ function Controls(props) {
   return (
     <div className="controls">
       <h2 className="title" >Controls</h2>
-      {props.start === true
+      <div className="buttons" >
+        {props.start === true
             ? <button className="stop" onClick={props.stopGame} >Stop</button> 
             : <button className="start" onClick={props.startGame} >Start</button> 
         }
-      <button className="step" onClick={props.gameTick} >Step</button>
-      <button className="reset" onClick={props.resetGame}>Reset</button>
+        <button className="step" onClick={props.gameTick} >Step</button>
+        <button className="reset" onClick={props.resetGame}>Reset</button>
+      </div>
+      
       <div className="generation">Generation: {props.generation}</div>
 
       <h2 className="presets" >Presets:</h2>
-      <button className="preset" onClick={props.generateGrid} >Random</button>
-      <button className="preset" onClick={props.blankGrid}>Blank</button>
-      <button className="preset" onClick={props.gunGrid}>Glider Gun</button>
+      <div className="buttons" >
+        <button className="preset" onClick={props.generateGrid} >Random</button>
+        <button className="preset" onClick={props.blankGrid}>Blank</button>
+        <button className="preset" onClick={props.gunGrid}>Glider Gun</button>
+      </div>
+      
 
         
     </div>
